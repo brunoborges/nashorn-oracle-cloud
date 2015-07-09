d10168935827ae6f56a2dac10c6cfe67e8815947
Bruno Borges <bruno.borges@gmail.com>
2015-07-09 12:58:32 -0500

    minor change

--- java/start-instance.js
+++ java/start-instance.js
@@ -1,18 +1,13 @@
-var startJson = {
-                  "lifecycleState"  : "start",
-                  "lifecycleTimeout" : "30" 
-               };
-
+var json = {"lifecycleState"  : "start"};
 var contentType = 'application/vnd.com.oracle.oracloud.provisioning.Service+json';
 var Entity = Packages.javax.ws.rs.client.Entity;
-var entity = Entity.entity(JSON.stringify(startJson), contentType);
-
+var entity = Entity.entity(JSON.stringify(json), contentType);
 var request = getTarget(credentials)
              .path(arguments[1])
              .request();
 
 var response = JSON.parse(
-  request.post(entity, java.lang.String.class)
+  request.post(entity, jString.class)
 );
 
 print(
