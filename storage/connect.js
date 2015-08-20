a42bdec2671e24e26f0271fcfeba95e2d5a1e9ae
Bruno Borges <bruno.borges@gmail.com>
2015-08-20 18:21:36 -0300

    do not use factory

--- storage/connect.js
+++ storage/connect.js
@@ -4,14 +4,28 @@ var identityDomain = credentials.identityDomain;
 var username = credentials.username; 
 var password = readFully(credentials.passwordFile).trim();
 var service = credentials.serviceName;
+var serviceUrl = credentials.serviceUrl;
 
 // Imports
-var CloudStorageConfig = Packages.oracle.cloud.storage.CloudStorageConfig;
-var CloudStorageFactory = Packages.oracle.cloud.storage.CloudStorageFactory;
+var CloudStorageConfig = Java.type("oracle.cloud.storage.CloudStorageConfig");
+var CloudStorageServiceProvider = Java.type("oracle.cloud.storage.internal.CloudStorageServiceProviderImpl");
 
-  myConfig = new CloudStorageConfig();
-  myConfig.setServiceName(service + "-" + identityDomain)
-   .setUsername(username)
-   .setPassword(new java.lang.String(password).toCharArray())
-   .setServiceUrl("https://storage.us2.oraclecloud.com");
-  storageConnection = CloudStorageFactory.getStorage(myConfig);
+function getStorage(myConfig) {
+  var provider = new CloudStorageServiceProvider();
+  provider.validateConfig(myConfig);
+  return provider.createCloudStorage(myConfig);
+}
+
+function getEncryptedStorage(myConfig) {
+  var provider = new CloudStorageServiceProvider();
+  provider.validateConfig(myConfig);
+  return provider.createEncryptedCloudStorage(myConfig);
+} 
+
+myConfig = new CloudStorageConfig();
+myConfig.setServiceName(service + "-" + identityDomain)
+ .setUsername(username)
+ .setPassword(new java.lang.String(password).toCharArray())
+ .setServiceUrl(serviceUrl);
+
+storageConnection = getStorage(myConfig);
--- storage/jjs.sh
+++ storage/jjs.sh
@@ -8,10 +8,13 @@
 # Then run $ ./jjs.sh your-script.js <arg0> <arg1> <arg2> <...>
 # 
 STORAGE_SDK=/home/bruno/Work/workspaces/oracle/cloud-dev-program/storage-cloud-service-sdk
+CLASSPATH=$STORAGE_SDK/lib/oracle.cloud.storage.api-13.0.0.jar
 
 if [ ! -d "$STORAGE_SDK" ]; then
   echo "Download Oracle Storage Cloud Service SDK from http://www.oracle.com/technetwork/topics/cloud/downloads/cloud-service-java-sdk-2121032.html"
   echo "Extract and adjust variable STORAGE_SDK inside jjs.sh"
 fi
 
-java -Dnashorn.args="-scripting" -cp classes:$STORAGE_SDK/lib/jersey-client-1.13.jar:$STORAGE_SDK/lib/jersey-core-1.13.jar:$STORAGE_SDK/lib/jersey-json-1.13.jar:$STORAGE_SDK/lib/jersey-multipart-1.13.jar:$STORAGE_SDK/lib/jettison-1.1.jar:$STORAGE_SDK/lib/mimepull-1.9.3.jar:$STORAGE_SDK/lib/oracle.cloud.storage.api-13.0.0.jar Execute $@
+# java -Dnashorn.args="-scripting" -cp classes:$CLASSPATH Execute $@
+
+jjs -scripting -cp $CLASSPATH $@
